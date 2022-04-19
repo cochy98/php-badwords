39119faa8dc2f69e1e8ff5add375e63103687c0d
cochy98 <cosmoferrigno@gmail.com>
2022-04-19 15:00:22 +0200

    Stampa paragrafo e la sua lunghezza

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
-$array = ['paolo', 'andrea', 'luca'];
-
-
+$paragraph = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu';
 ?>
 
-<pre>
-  <?php  var_dump($array); ?>
-</pre>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords</title>
+</head>
+<body>
+  <p>
+    <?php  var_dump($paragraph) ?>
+  </p>
+</body>
+</html>
